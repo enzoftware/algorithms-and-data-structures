cb7012be1bc740aa6537211a295f2e7532f6fb22
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2017-06-01 21:45:27 -0500

    p10327

--- Unidad3/Ordenamiento/p10327.cpp
+++ Unidad3/Ordenamiento/p10327.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int isSorted(int a , int b){
-	return a-b;
-}
 
 int main(){
+
+	int n;
+	while(cin>>n){
+		vector<int> v;
+		int count = 0,aux;
+		for(int i = 0 ; i<n ; i++){
+			cin>>aux;
+			v.push_back(aux);
+		}
+
+		for(int i = n-1 ; i>=0 ; i--){
+			for(int j = 0 ; j< i ; j++){
+				if(v[j] > v[j+1]){
+					swap(v[j],v[j+1]);
+					count++;
+				}
+			}
+		}
+
+		cout<<"Minimum exchange operations : "<<count<<endl;
+
+	}
+
 
 	return 0;
 }

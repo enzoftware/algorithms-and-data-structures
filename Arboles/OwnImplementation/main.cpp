30524a2b1f3ea44f17ba5e2914f74ff84a2153f4
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2017-07-08 21:02:52 -0500

    avl is coming

--- Arboles/OwnImplementation/main.cpp
+++ Arboles/OwnImplementation/main.cpp
@@ -12,5 +12,44 @@ int compareElements(int a , int b){
 
 int main(){
   BinaryTree<int>* btree = new BinaryTree<int>(printTree,compareElements);
+  btree->insertElement(5);
+  btree->insertElement(52);
+  btree->insertElement(1);
+  btree->insertElement(100);
+  btree->insertElement(5);
+  btree->insertElement(5);
+  btree->insertElement(5);
+  btree->insertElement(5);
+  btree->insertElement(50000);
+  btree->insertElement(10);
+  btree->OrderTree();
+  cout<<endl;
+  btree->deleteElement(1);
+  btree->OrderTree();
   return 0;
 }
+/*
+int hl = _heightTree(node->leftSon);
+int hr = _heightTree(node->rightSon);
+int d = hr-hl;
+if(d > 1){ // heavy to right
+    int hnl = _heightTree(node->rightSon->leftSon);
+    int hnr = _heightTree(node->rightSon->rightSon);
+    if(hnl > hnr){ // zigzag rig to lef
+        _rotateRight(node->rightSon->leftSon, node->rightSon , node->rightSon);
+    }
+    _rotateLeft(node,node->rightSon,node);
+}
+else if(d < -1){ //heavy to left
+    int hnl = _heightTree(node->leftSon->leftSon);
+    int hnr = _heightTree(node->leftSon->rightSon);
+    if(hnr > hnl){ //zigzag lef to rig
+      _rotateLeft(node->leftSon,node->leftSon->rightSon, node->leftSon);
+    }
+    _rotateRight(node->leftSon,node,node);
+}
+hl = _heightTree(node->leftSon);
+hr = _heightTree(node->rightSon);
+node->height =1+ (hl > hr ? hl : hr);
+return true;
+*/
--- Arboles/OwnImplementation/treeavl.h
+++ Arboles/OwnImplementation/treeavl.h
@@ -3,30 +3,37 @@ template<class T>
 struct Node{
   T element;
   Node* leftSon;
-  Node* rightSon;
+  Node* rightSon ;
   int height;
+  Node(){
+     height = 0;
+     rightSon = NULL;
+     leftSon = NULL;
+  }
 };
 
 template<class T>
 class BinaryTree{
   Node<T>* root;
-  void (*process)(T);
-  int (*compare)(T,T);
+  void (*process)(T);  // Templates of fuctions that print and
+  int (*compare)(T,T);// compare to be easy to implement.
 
   bool _insert(Node<T>*& node , T e){
     if(node == NULL){
       node = new Node<T>;
       node->element = e;
     }else if(compare(e, node->element) < 0){
-      _insertar(node->leftSon , e);
+      _insert(node->leftSon , e);
     }else{
-      _insertar(node->rightSon,e);
+      _insert(node->rightSon,e);
     }
+
+
   }
 
 int _heightTree(Node<T>* node){
   if(node == NULL) return -1;
-  return node->h;
+  return node->height;
 }
 
 T _search(Node<T>* node , T e){
@@ -44,10 +51,109 @@ void _inOrder(Node<T>* node){
   process(node->element);
   _inOrder(node->rightSon);
 }
+
+//-><--><--><--><--><--><--><--><--><--><--><--><--><--><--><--><--><--><--><--><--><-
+//TODO : Delete a node and balance the tree
+//-><--><--><--><--><--><--><--><--><--><--><--><--><--><--><--><--><--><--><--><--><-
+
+void _rotateLeft(Node<T>*& x ,Node<T>*& y ,Node<T>*& father ){
+  father = y;
+  x->rightSon = y->leftSon;
+  father->leftSon = x;
+}
+
+void _rotateRight(Node<T>*& x ,Node<T>*& y ,Node<T>*& father){
+  father = x;
+  y->leftSon = x->rightSon;
+  father->rightSon = y;
+}
+
+
+Node<T>* _quitMin(Node<T>*node , Node<T>*& vinc){
+  if(node == NULL) return NULL;
+  if(node->leftSon == NULL){ // Alredy found the min
+      vinc = node->rightSon;
+      node->rightSon = NULL;
+      return node;
+  }else{
+    return _quitMin(node->leftSon,node->leftSon);
+  }
+}
+
+  Node<T>* _quitMax(Node<T>*node , Node<T>*& vinc){
+    if(node == NULL) return NULL;
+    if(node->rightSon == NULL){ // Alredy found the min
+        vinc = node->leftSon;
+        node->leftSon = NULL;
+        return node;
+    }else{
+      return _quitMax(node->rightSon,node->rightSon);
+    }
+}
+
+bool _deleteThis(T e , Node<T>* node , Node<T>*& vinc){
+    if(compare(e,node->element) == 0){
+      int hl = _heightTree(node->leftSon);
+      int hr = _heightTree(node->rightSon);
+      if(hl == -1 && hr == -1){
+        vinc = NULL;
+        delete node;
+        return true;
+      }
+      else if(hl > hr){
+        Node<T>* replace = _quitMax(node->leftSon,node->leftSon);
+        replace->leftSon = node->leftSon;
+        replace->rightSon = node->rightSon;
+        vinc = replace;
+        delete node;
+      }
+      else{
+        Node<T>* replace = _quitMin(node->rightSon, node->rightSon);
+        replace->leftSon = node->leftSon;
+        replace->rightSon = node->rightSon;
+        vinc = replace;
+        delete node;
+      }
+    }
+    else if(compare(e,node->element) > 0 && node->rightSon != NULL){
+      return _deleteThis(e,node->rightSon, node->rightSon);
+    }
+    else if(compare(e,node->element) > 0 && node->leftSon != NULL){
+      return _deleteThis(e,node->leftSon,node->leftSon);
+    }
+    else{
+      return false;
+    }
+}
+
+
+
 public:
   BinaryTree(void (*processF)(T),int (*compareF)(T,T) ){
     this->process = processF;
     this->compare = compareF;
     root = NULL;
   }
+
+  bool insertElement(T e){
+    return _insert(root,e);
+  }
+
+  void OrderTree(){
+    _inOrder(root);
+  }
+
+  int getHeight(){
+    _heightTree(root);
+  }
+
+  T searchElement(T e){
+    return _search(root,e);
+  }
+
+  bool deleteElement(T e){
+    return _deleteThis(e,root,root);
+  }
+
+
 };
